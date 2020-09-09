7a287bf9d5bfee718476579b5c59b77062332d84
Yuri Becker <becker.yr@gmail.com>
2020-09-09 19:14:02 -0300

    Add firebase config

---
+++ src/config/firebase/index.ts
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyAY7h-JwfDqsPyhJgHd7PTgn-mBlYtsFHY',
+  authDomain: 'challenge-2aa10.firebaseapp.com',
+  databaseURL: 'https://challenge-2aa10.firebaseio.com',
+  projectId: 'challenge-2aa10',
+  storageBucket: 'challenge-2aa10.appspot.com',
+  messagingSenderId: '227410851513',
+  appId: '1:227410851513:web:4f01260e3faeea6f61cd68'
+}
+
+export default firebaseConfig
