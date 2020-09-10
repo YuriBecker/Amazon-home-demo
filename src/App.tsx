2bb943eff6fef7d9e172c679ff723aa240911a7d
Yuri Becker <becker.yr@gmail.com>
2020-09-10 11:12:09 -0300

    Fix eslint error

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import {
+  AspectRatioBox,
+  Button,
+  CSSReset,
+  Image,
+  ThemeProvider
+} from '@chakra-ui/core'
 import React, { FC } from 'react'
-import { ThemeProvider, CSSReset, Button } from '@chakra-ui/core'
 import customTheme from './styles/theme'
 
 const App: FC = () => {
@@ -7,6 +13,13 @@ const App: FC = () => {
     <ThemeProvider theme={customTheme}>
       <CSSReset />
       <Button>Hello</Button>
+      <AspectRatioBox w={{ base: 1 / 2, xl: '100%' }} ratio={21 / 9}>
+        <Image
+          src="https://bit.ly/naruto-sage"
+          alt="naruto"
+          objectFit="cover"
+        />
+      </AspectRatioBox>
     </ThemeProvider>
   )
 }
