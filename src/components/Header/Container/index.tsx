503d822eff4fb67f0f39b3849a99a11275ec1d9c
Yuri Becker <becker.yr@gmail.com>
2020-09-14 19:12:35 -0300

    Add header container

---
+++ src/components/Header/Container/index.tsx
@@ -0,0 +1,21 @@
+import React, { FC } from 'react'
+import { Flex } from '@chakra-ui/core'
+
+export const Container: FC = ({ children }) => {
+  return (
+    <Flex
+      direction="row"
+      minH="60px"
+      alignItems="center"
+      background="#131921"
+      position="sticky"
+      top={0}
+      zIndex={100}
+      px="10px"
+      wrap="wrap"
+      justify="center"
+    >
+      {children}
+    </Flex>
+  )
+}
