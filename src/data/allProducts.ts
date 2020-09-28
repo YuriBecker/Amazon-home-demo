39426fb8b108352aaf50061971aa5edcc764d0cc
Yuri Becker <becker.yr@gmail.com>
2020-09-28 19:01:02 -0300

    Add products mock data

---
+++ src/data/allProducts.ts
@@ -0,0 +1,79 @@
+import { Product } from '../interfaces/Product'
+
+export const allProducts: Product[] = [
+  {
+    name: 'DualSense Wireless Controller',
+    price: 69.99,
+    imageUrl:
+      'https://images-na.ssl-images-amazon.com/images/I/71y%2BUGuJl5L._SX425_.jpg',
+    stars: 4,
+    numVotes: 1347,
+    brand: 'PlayStation'
+  },
+  {
+    name: 'Nintendo Switch with Neon Blue and Neon Red',
+    price: 299.99,
+    imageUrl:
+      'https://images-na.ssl-images-amazon.com/images/I/61JnrafZ7zL._AC_SX679_.jpg',
+    stars: 5,
+    numVotes: 27359,
+    brand: 'Nintendo'
+  },
+  {
+    name: 'Elite Series 2 Controller - Black',
+    price: 163.75,
+    imageUrl:
+      'https://images-na.ssl-images-amazon.com/images/I/71F6eID-ImL._SX522_.jpg',
+    stars: 4,
+    numVotes: 8101,
+    brand: 'Microsoft'
+  },
+  {
+    name:
+      "Marvel's Spider-Man: Miles Morales Ultimate Launch Edition – PlayStation 5",
+    price: 69.99,
+    imageUrl:
+      'https://images-na.ssl-images-amazon.com/images/I/81TG7TjMzYL._SX425_.jpg',
+    stars: 0,
+    numVotes: 0,
+    brand: 'PlayStation'
+  },
+  {
+    name: 'Demon’s Souls – PlayStation 5',
+    price: 69.99,
+    imageUrl:
+      'https://images-na.ssl-images-amazon.com/images/I/81cGD6VkiQL._SX425_.jpg',
+    stars: 0,
+    numVotes: 0,
+    brand: 'PlayStation'
+  },
+  {
+    name: 'Cyberpunk 2077 - Xbox One',
+    price: 69.99,
+    imageUrl:
+      'https://images-na.ssl-images-amazon.com/images/I/81KJ8RgF3GL._SX385_.jpg',
+    stars: 0,
+    numVotes: 0,
+    brand: 'WB Games'
+  },
+  {
+    name:
+      'AOC CU34G2X 34 Curved Frameless Immersive Gaming Monitor, UltraWide QHD 3440x1440, VA Panel, 1ms 144Hz Freesync, Height Adjustable, 3-Yr Zero Dead Pixels',
+    price: 449.99,
+    imageUrl:
+      'https://images-na.ssl-images-amazon.com/images/I/61-ke7Qp%2BxL._AC_SX679_.jpg',
+    stars: 5,
+    numVotes: 7952,
+    brand: 'AOC'
+  },
+  {
+    name:
+      'Microsoft Xbox One X 1Tb Console With Wireless Controller: Xbox One X Enhanced, Hdr, Native 4K, Ultra Hd (Discontinued)',
+    price: 554.0,
+    imageUrl:
+      'https://images-na.ssl-images-amazon.com/images/I/61ux1cU49XL._SX522_.jpg',
+    stars: 4,
+    numVotes: 3265,
+    brand: 'Microsoft'
+  }
+]
