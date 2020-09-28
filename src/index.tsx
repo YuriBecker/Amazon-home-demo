4452bea6699a221ea206b6bd86fc0ca1828026a9
Yuri Becker <becker.yr@gmail.com>
2020-09-28 19:06:39 -0300

    Update global styles location

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import '../public/globalStyles.css'
+import './styles/globalStyles.css'
 
 ReactDOM.render(
   <React.StrictMode>
