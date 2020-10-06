e4aa148be73370e34cd437c2cca387f72db01077
Yuri Becker <becker.yr@gmail.com>
2020-10-06 18:07:48 -0300

    Add app router

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,21 @@ import { CSSReset, ThemeProvider } from '@chakra-ui/core'
 import React, { FC } from 'react'
 import Header from './components/Header'
 import customTheme from './styles/theme'
-import { Home } from './components/Home'
+import { Home } from './Pages/Home'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { Error404 } from './Pages/Error404'
 
 const App: FC = () => {
   return (
     <ThemeProvider theme={customTheme}>
       <CSSReset />
-      <Header />
-      <Home />
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route component={Error404} />
+        </Switch>
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
