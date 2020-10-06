1bbc320a93b0c223e02e73e1f27cba6bde204c8f
Yuri Becker <becker.yr@gmail.com>
2020-10-06 18:08:07 -0300

    Update header logo

--- src/components/Header/Logo/index.tsx
+++ src/components/Header/Logo/index.tsx
@@ -1,17 +1,20 @@
 import React, { FC } from 'react'
-import { Image, Box } from '@chakra-ui/core'
+import { Image, Box, Link } from '@chakra-ui/core'
 import LogoImg from '../../../assets/images/logo.png'
-
+import { Link as LinkRouter } from 'react-router-dom'
 export const Logo: FC = () => {
   return (
     <Box marginY={{ base: '10px', md: '0px' }}>
-      <Image
-        src={LogoImg}
-        alt="logo"
-        objectFit="contain"
-        width="150px"
-        height="30px"
-      />
+      {/* @ts-ignore */}
+      <Link as={LinkRouter} to="/" _focus={{ border: 'none' }}>
+        <Image
+          src={LogoImg}
+          alt="logo"
+          objectFit="contain"
+          width="150px"
+          height="30px"
+        />
+      </Link>
     </Box>
   )
 }
