4010918ab07ccac8b5fb2e30e3bedce256741aa9
Yuri Becker <becker.yr@gmail.com>
2020-11-19 18:35:56 -0300

    Update header

--- src/components/Header/Cart/index.tsx
+++ src/components/Header/Cart/index.tsx
@@ -1,32 +1,39 @@
 import React, { FC } from 'react'
-import { Text, PseudoBox, Box, Flex } from '@chakra-ui/core'
+import { Text, PseudoBox, Box, Flex, Link } from '@chakra-ui/core'
 import { IoMdCart } from 'react-icons/io'
-
+import { Link as LinkRouter } from 'react-router-dom'
 interface Props {
   numItems: number
 }
 
 export const Cart: FC<Props> = ({ numItems }) => {
   return (
-    <PseudoBox
-      px="9px"
-      borderRadius="sm"
-      _hover={{ border: '1px solid white', padding: '8px' }}
-      display="flex"
-      flexDir="row"
-      cursor="pointer"
-      alignItems="center"
-    >
-      <Box as={IoMdCart} size="32px" color="white" />
-      <Flex direction="column" align="center" justify="center">
-        <Text fontSize="12px" color="#ff9900" fontWeight="bold">
-          {numItems}
-        </Text>
-        <Text fontSize="14px" fontWeight="bold" color="white" ml={1}>
-          Cart
-        </Text>
-      </Flex>
-      <Flex align="flex-end"></Flex>
-    </PseudoBox>
+    <>
+      {/* @ts-ignore */}
+      <Link as={LinkRouter} to="/checkout" _focus={{ border: 'none' }}>
+        <PseudoBox
+          px="9px"
+          borderRadius="sm"
+          _hover={{ border: '1px solid white', padding: '8px' }}
+          display="flex"
+          flexDir="row"
+          cursor="pointer"
+          alignItems="center"
+        >
+          <Box as={IoMdCart} size="32px" color="white" />
+          <Flex direction="column" align="center" justify="center">
+            {numItems !== 0 && (
+              <Text fontSize="12px" color="#ff9900" fontWeight="bold">
+                {numItems}
+              </Text>
+            )}
+            <Text fontSize="14px" fontWeight="bold" color="white" ml={1}>
+              Cart
+            </Text>
+          </Flex>
+          <Flex align="flex-end"></Flex>
+        </PseudoBox>
+      </Link>
+    </>
   )
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,24 @@
-import React, { FC } from 'react'
+import React, { FC, useContext } from 'react'
 import { Logo } from './Logo'
 import { Search } from './Search'
 import { SignIn } from './SignIn'
 import { Orders } from './Orders'
 import { Container } from './Container'
 import { Cart } from './Cart'
+import { AppContext } from '../../context'
 
 const Header: FC = () => {
+  const { state: CartProducts } = useContext(AppContext)
+
+  const numOfItems = CartProducts.length
+
   return (
     <Container>
       <Logo />
       <Search />
       <SignIn />
       <Orders />
-      <Cart numItems={15} />
+      <Cart numItems={numOfItems} />
     </Container>
   )
 }
