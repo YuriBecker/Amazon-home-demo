830accf4d6498e13d23080376b3a796b368f0b23
Yuri Becker <becker.yr@gmail.com>
2020-11-19 18:36:22 -0300

    Add product ids

--- src/data/allProducts.ts
+++ src/data/allProducts.ts
@@ -2,6 +2,7 @@ import { Product } from '../interfaces/Product'
 
 export const allProducts: Product[] = [
   {
+    id: 1,
     name: 'DualSense Wireless Controller',
     price: 69.99,
     imageUrl:
@@ -11,6 +12,7 @@ export const allProducts: Product[] = [
     brand: 'PlayStation'
   },
   {
+    id: 2,
     name: 'Nintendo Switch with Neon Blue and Neon Red',
     price: 299.99,
     imageUrl:
@@ -20,6 +22,7 @@ export const allProducts: Product[] = [
     brand: 'Nintendo'
   },
   {
+    id: 3,
     name: 'Elite Series 2 Controller - Black',
     price: 163.75,
     imageUrl:
@@ -29,6 +32,7 @@ export const allProducts: Product[] = [
     brand: 'Microsoft'
   },
   {
+    id: 4,
     name:
       "Marvel's Spider-Man: Miles Morales Ultimate Launch Edition – PlayStation 5",
     price: 69.99,
@@ -39,6 +43,7 @@ export const allProducts: Product[] = [
     brand: 'PlayStation'
   },
   {
+    id: 5,
     name: 'Demon’s Souls – PlayStation 5',
     price: 69.99,
     imageUrl:
@@ -48,6 +53,7 @@ export const allProducts: Product[] = [
     brand: 'PlayStation'
   },
   {
+    id: 6,
     name: 'Cyberpunk 2077 - Xbox One',
     price: 69.99,
     imageUrl:
@@ -57,6 +63,7 @@ export const allProducts: Product[] = [
     brand: 'WB Games'
   },
   {
+    id: 7,
     name:
       'AOC CU34G2X 34 Curved Frameless Immersive Gaming Monitor, UltraWide QHD 3440x1440, VA Panel, 1ms 144Hz Freesync, Height Adjustable, 3-Yr Zero Dead Pixels',
     price: 449.99,
@@ -66,6 +73,7 @@ export const allProducts: Product[] = [
     brand: 'AOC'
   },
   {
+    id: 8,
     name:
       'Microsoft Xbox One X 1Tb Console With Wireless Controller: Xbox One X Enhanced, Hdr, Native 4K, Ultra Hd (Discontinued)',
     price: 554.0,
