19cc24534d8f38eb3e29318e5c19d59502c5db6f
Yuri Becker <becker.yr@gmail.com>
2020-12-18 15:49:45 -0300

    Add deleteAll in reducer

--- src/reducers/index.tsx
+++ src/reducers/index.tsx
@@ -13,12 +13,14 @@ type ActionMap<M extends { [index: string]: any }> = {
 
 export enum Types {
   Delete = 'DELETE_PRODUCT',
+  DeleteAll = 'DELETE_ALL_PRODUCTS',
   Add = 'ADD_PRODUCT'
 }
 
 type ProductPayload = {
   [Types.Add]: Product
   [Types.Delete]: number
+  [Types.DeleteAll]: undefined
 }
 
 export type ProductActions = ActionMap<ProductPayload>[keyof ActionMap<
@@ -36,6 +38,10 @@ export const productReducer = (
     case Types.Delete: {
       const idToDelete = action.payload
       return [...state.filter(product => product.id !== idToDelete)]
+    }
+
+    case Types.DeleteAll: {
+      return []
     }
 
     default:
